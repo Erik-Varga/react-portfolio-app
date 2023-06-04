a8826e7a352a0e9173b31a2d54612e65f73b0a76
Erik Varga <viglew@gmail.com>
2023-06-04 10:55:06 -0400

    configured navbar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './navbar.css'
+import data from './data'
+import { IoIosColorPalette } from 'react-icons/io'
 
 const Navbar = () => {
   return (
     <section id='navbar'>
-        <h2>Navbar</h2>
+        <nav>
+          <div className="container nav__container">
+            <a href="index.html" className='nav__logo'>
+              <img src="https://avatars.githubusercontent.com/u/67766949?v=4" alt="" />
+            </a>
+            <ul className='nav__menu'>
+              {
+                data.map(item => <li><a key={item.id} href={item.link}>{item.title}</a></li>)
+              }
+            </ul>
+            <button id='theme__icon'><IoIosColorPalette /></button>
+          </div>
+        </nav>
     </section>
   )
 }
---
+++ src/sections/navbar/data.js
@@ -0,0 +1,10 @@
+// data array
+const data = [
+    {id: 1, link: '#', title: 'Home'},
+    {id: 2, link: '#', title: 'About'},
+    {id: 3, link: '#', title: 'Services'},
+    {id: 4, link: '#', title: 'Portfolio'},
+    {id: 5, link: '#', title: 'Contact'}
+]
+
+export default data;
