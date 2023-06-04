1b2a2c05ad767059421611006c5cb4ed2f9cdc27
Erik Varga <viglew@gmail.com>
2023-06-04 07:01:17 -0400

    react setup files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Erik's Portfolio App</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
